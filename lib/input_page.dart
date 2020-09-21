1f511d530538bb711c91a72ecddbb48ca866f81e
HamzaMihfad <mihfad1hamza@gmail.com>
2020-09-21 09:19:14 +0100

    using const

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 65.0;
+const bottomContainerColor = Color(0xffeb1555);
+const cardColor = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,48 +13,55 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('BMI CALCULATOR'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Card1(cardColor: Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: Card1(cardColor: Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Card1(cardColor: cardColor),
+                ),
+                Expanded(
+                  child: Card1(cardColor: cardColor),
+                ),
+              ],
             ),
-            Expanded(
-              child: Card1(cardColor: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Card1(cardColor: cardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Card1(cardColor: cardColor),
+                ),
+                Expanded(
+                  child: Card1(cardColor: cardColor),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Card1(cardColor: Color(0xFF1D1E33)),
-                  ),
-                  Expanded(
-                    child: Card1(cardColor: Color(0xFF1D1E33)),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
+      ),
+    );
   }
 }
 
 class Card1 extends StatelessWidget {
   Card1({@required this.cardColor});
 
-  Color cardColor;
+  final Color cardColor;
 
   @override
   Widget build(BuildContext context) {
